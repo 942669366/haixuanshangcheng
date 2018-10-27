d5f47b5826234af63af38934e4b3b00fdf4e6143
asus <942669366@qq.com>
2018-10-27 23:00:39 +0800

    二次

--- Jindongg/sqlapp/urls.py
+++ Jindongg/sqlapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from sqlapp import views
 
 urlpatterns = [
-    url(r'^BOSS/$', views.BOSS),
+    url(r'^$', views.BOSS),
     url(r'^login/',views.login),
     url(r'^regist/',views.regist),
     url(r'^flow1/',views.flow1),
